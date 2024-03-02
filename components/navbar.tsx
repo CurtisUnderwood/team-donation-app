d5fbd0499543c41adc9db42ef90beb1d2c9713de
CurtisUnderwood <curtisund@gmail.com>
2024-03-02 02:10:48 -0500

    feat: enhanced navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,84 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul className="flex justify-between items-center mx-5">
-        <li>
+    <nav className="text-forest-green font-semibold py-4 ml-10">
+      <div className="flex items-center">
+        <div className="flex items-center">
+          {!isOpen && ( // Display the links when the menu is closed
+            <ul className="hidden md:flex space-x-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/game">Games</Link>
+              </li>
+              <li>
+                <Link href="/leaderboard">Leaderboard</Link>
+              </li>
+              <li>
+                <Link href="/">Blog</Link>
+              </li>
+            </ul>
+          )}
+        </div>
+        <button onClick={toggleMenu} className="md:hidden">
+            {/* Hamburger menu icon */}
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+          <div className={`mx-auto ${isOpen ? '' : 'pr-20'}`}>
           <Link href="/">
-            <p>Home</p>
+              <img src="/logo.png" alt="Logo" className="h-12" />
           </Link>
-        </li>
-        <li>
-          <Link href="/game">
-            <p>Game</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/profile">
-            <p>Profile</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/leaderboard">
-            <p>Leaderboard</p>
-          </Link>
-        </li>  
-        <li>
-          <Link href="/login">
-            <p>Login</p>
-          </Link>
-        </li>   
-      </ul>
+        </div>
+        <div className="flex items-center space-x-4">
+            <Link href="/login">Log In</Link>
+            <Link href="/signup">
+              <button className="bg-white text-forest-green border-2 border-forest-green rounded-xl px-4 py-2 mr-10 hover:bg-forest-green hover:text-white">
+                Sign Up
+              </button>
+            </Link>
+        </div>
+      </div>
+      {/* Mobile view menu */}
+      {isOpen && (
+        <div className="md:hidden">
+          <ul className="flex flex-col space-y-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/game">Games</Link>
+            </li>
+            <li>
+              <Link href="/leaderboard">Leaderboard</Link>
+            </li>
+            <li>
+              <Link href="/">Blog</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
