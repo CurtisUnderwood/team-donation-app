2d075415a4322cf1d26bae072c0ebf6ee20940e1
wise-bit <satrajit2016@gmail.com>
2024-03-03 03:34:52 -0500

    variable amount increment

--- components/IncrementScore.tsx
+++ components/IncrementScore.tsx
@@ -1,15 +1,15 @@
 import { db } from "@/firebaseConfig";
 import { doc, updateDoc, increment } from "firebase/firestore";
 
-async function incrementUserScore(user: any) {
+async function incrementUserScore(user: any, amount: number = 0.01) {
   const firebaseName = (user.username ?? user.nickname).toString();
   const userDocRef = doc(db, "yourCollectionName", firebaseName);
 
   try {
     await updateDoc(userDocRef, {
-      score: increment(0.01),
+      score: increment(amount),
     });
-    console.log(`Score of ${firebaseName} incremented by 0.01.`);
+    console.log(`Score of ${firebaseName} incremented by ${amount}.`);
   } catch (error) {
     console.error("Error incrementing user's score:", error);
   }
